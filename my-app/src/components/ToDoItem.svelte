56efc5c7c6019d246fc93fd1dc750ea68e736d0d
fira zem fira <144517701+Zemfira-B@users.noreply.github.com>
2023-10-25 11:59:13 +0500

    Add files via upload

---
+++ my-app/src/components/ToDoItem.svelte
@@ -0,0 +1,51 @@
+<script>
+    export let id;
+    export let text;
+    export let isDone;
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    
+</script>
+<div class="todo-item" class:todo-item__done={isDone}>
+    <input 
+       class="todo-item__check"
+       type="checkbox"
+       on:change={dispatch('change', {id})}
+       checked ={isDone}
+    />
+    <span class="todo-item__text">{text}</span>
+    <button class="todo-item__btn" on:click={dispatch('remove', {id})} >Del</button>
+</div>
+
+<style>
+    .todo-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15px;
+        background-color: rgb(233, 157, 157);
+        border-radius: 10px;
+    }
+    .todo-item__done {
+        background: rgb(159, 236, 159);
+    }
+
+    .todo-item__done .todo-item__text {
+        text-decoration: line-through;
+    }
+
+    .todo-item__check {
+        width: 20px;
+    }
+    .todo-item__text{
+        flex-grow: 1;
+    }
+    .todo-item__btn {
+        border: none;
+        border-radius: 10px;
+        background-color: chocolate;
+        padding: 10px;
+        min-width: 100px;
+    }
+</style>
